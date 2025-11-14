441fe8f29719c0be599fd957c1fe03d8f85867b6
Laratatouile <maxpublic09@gmail.com>
2025-11-14 09:52:44 +0100

    14/11

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # TNSI_projet_3
 project 3 de NSI en terminale
+
+
+
+
+
+
+# parking
---
+++ parking.py
@@ -0,0 +1,81 @@
+import random
+
+class Parking:
+    def __init__(self):
+        self.dict_abo = {}
+        self.liste_place_abo_libres = []
+        self.parking = {f"{etage}" : {f"{etage}{i}" : None for i in range(80)} for etage in range(80)}
+
+
+
+    def ajouter_voiture(self, voiture:object) -> bool:
+        """ ajoute une voiture sur le parking """
+        # si la voiture est abonee
+        if voiture in self.dict_abo.keys():
+            place = str(self.dict_abo[voiture])
+            if self.parking[place] == None:
+                self.parking[place] = voiture
+            else:
+                place = self.__premiere_place_libre()
+                self.parking[place] = voiture
+        # si elle n'est pas abonee
+        else:
+            self.parking[str(self.__premiere_place_libre())] = voiture
+    
+
+
+
+    def place_vide(self, place:int) -> any:
+        """ renvoie si une place est vide ou non """
+        if type(self.parking[str(place)]) == None:
+            return False
+        return self.parking[str(place)]
+    
+
+
+
+
+    def abonnement(self, voiture:object) -> None:
+        """ abone une voiture """
+        if self.liste_place_abo_libres != []:
+            self.dict_abo[voiture] = self.liste_place_abo_libres.pop(0)
+        else:
+            self.dict_abo[voiture] = self.__premiere_place_libre()
+
+
+    def desabonnement(self, voiture:object) -> None:
+        """ retire l'abonnement d'une voiture """
+        for voiture in self.dict_abo[]
+        
+
+
+
+
+
+    def __premiere_place_libre(self):
+        """ renvoie la premiere place libre du parking """
+        for place, voiture in self.parking.items():
+            if voiture == None:
+                return place
+        return False
+
+
+
+
+    
+    def affiche_etage(self, numero):
+        for place, voiture in self.parking.items(): # place str , voiture objet
+            # verifier si le premier chiffre de la place est egal a notre numero d'etage faire attention aux types int et str
+            # si la place est dans l'etage
+            # modifier la case de la liste par True
+            # afficher la liste
+            pass
+        
+    
+
+
+
+
+
+
+
